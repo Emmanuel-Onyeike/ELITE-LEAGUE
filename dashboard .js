f96cc9b60a813e4573bc47c855089f99eb28630b
Emmanuel Onyeike <emmanuelonyeike33@gmail.com>
2026-01-17 20:55:39 +0100

    Update dashboard .js

--- dashboard .js
+++ dashboard .js
@@ -222,20 +222,21 @@ const contentData = {
                             <th class="py-5 text-right pr-8 text-white">PTS</th>
                         </tr>
                     </thead>
-                    <tbody class="text-gray-300 font-bold divide-y divide-white/[0.03]">
-                        ${[
-                            { rank: 1, team: "MSS-200", mp: 3, w: 2, d: 1, l: 0, gf: 18, ga: 11, gd: "+7", pts: 7, crown: true },
-                            { rank: 2, team: "CSC-200", mp: 5, w: 2, d: 1, l: 2, gf: 15, ga: 21, gd: "-6", pts: 7 },
-                            { rank: 3, team: "CSC-400", mp: 2, w: 2, d: 0, l: 0, gf: 12, ga: 6, gd: "+6", pts: 6 },
-                            { rank: 4, team: "CSC-100", mp: 5, w: 2, d: 0, l: 3, gf: 16, ga: 17, gd: "-1", pts: 6 },
-                            { rank: 5, team: "NUR-200", mp: 2, w: 1, d: 1, l: 0, gf: 9, ga: 6, gd: "+3", pts: 4 },
-                            { rank: 6, team: "LAW-200", mp: 2, w: 1, d: 1, l: 0, gf: 12, ga: 11, gd: "+1", pts: 4 },
-                            { rank: 7, team: "LAW-100", mp: 2, w: 1, d: 0, l: 1, gf: 11, ga: 10, gd: "+1", pts: 3 },
-                            { rank: 8, team: "MSS-100", mp: 3, w: 1, d: 0, l: 2, gf: 7, ga: 9, gd: "-2", pts: 3 },
-                            { rank: 9, team: "MSS-300", mp: 1, w: 0, d: 0, l: 1, gf: 5, ga: 7, gd: "-2", pts: 0 },
-                            { rank: 10, team: "MED-200", mp: 1, w: 0, d: 0, l: 1, gf: 4, ga: 7, gd: "-3", pts: 0 },
-                            { rank: 11, team: "CSC-300", mp: 1, w: 0, d: 0, l: 1, gf: 1, ga: 5, gd: "-4", pts: 0 },
-                            { rank: 12, team: "MED-500", mp: 0, w: 0, d: 0, l: 0, gf: 0, ga: 0, gd: "0", pts: 0 }
+                   <tbody class="text-gray-300 font-bold divide-y divide-white/[0.03]">
+    ${[
+        { rank: 1, team: "CSC-400", mp: 3, w: 3, d: 0, l: 0, gf: 22, ga: 8, gd: "+14", pts: 9, crown: true },
+        { rank: 2, team: "MSS-200", mp: 3, w: 2, d: 1, l: 0, gf: 18, ga: 11, gd: "+7", pts: 7 },
+        { rank: 3, team: "CSC-200", mp: 5, w: 2, d: 1, l: 2, gf: 15, ga: 21, gd: "-6", pts: 7 },
+        { rank: 4, team: "CSC-100", mp: 5, w: 2, d: 0, l: 3, gf: 16, ga: 17, gd: "-1", pts: 6 },
+        { rank: 5, team: "NUR-200", mp: 2, w: 1, d: 1, l: 0, gf: 9, ga: 6, gd: "+3", pts: 4 },
+        { rank: 6, team: "LAW-200", mp: 3, w: 1, d: 1, l: 1, gf: 14, ga: 21, gd: "-7", pts: 4 },
+        { rank: 7, team: "LAW-100", mp: 2, w: 1, d: 0, l: 1, gf: 11, ga: 10, gd: "+1", pts: 3 },
+        { rank: 8, team: "MSS-100", mp: 3, w: 1, d: 0, l: 2, gf: 7, ga: 9, gd: "-2", pts: 3 },
+        { rank: 9, team: "MED-500", mp: 0, w: 0, d: 0, l: 0, gf: 0, ga: 0, gd: "0", pts: 0 },
+        { rank: 10, team: "MSS-300", mp: 1, w: 0, d: 0, l: 1, gf: 5, ga: 7, gd: "-2", pts: 0 },
+        { rank: 11, team: "MED-200", mp: 1, w: 0, d: 0, l: 1, gf: 4, ga: 7, gd: "-3", pts: 0 },
+        { rank: 12, team: "CSC-300", mp: 1, w: 0, d: 0, l: 1, gf: 1, ga: 5, gd: "-4", pts: 0 }
+    ].map((item, index) => `
                         ].map((data) => `
                             <tr class="hover:bg-blue-600/[0.03] transition-colors group">
                                 <td class="py-5 pl-8">
