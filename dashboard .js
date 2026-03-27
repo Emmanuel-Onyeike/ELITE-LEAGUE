d608d8181198e9dd94edeb6cb2d5889e4ba7d0bb
Emmanuel Onyeike <emmanuelonyeike33@gmail.com>
2026-03-27 22:12:28 +0100

    Update dashboard .js

--- dashboard .js
+++ dashboard .js
@@ -131,7 +131,7 @@ const contentData = {
            bg-clip-text text-transparent 
            drop-shadow-[0_0_6px_rgba(255,0,0,0.35)] 
            animate-pulse">
-  C-LAW CLÁSICO 
+CIT DERBY
 </p>
 
                     <button onclick="updateView('Live')" class="group relative px-10 py-4 bg-transparent border border-blue-500/30 overflow-hidden rounded-2xl transition-all">
